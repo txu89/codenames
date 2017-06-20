2700874cfbf057c4028185b2a0da35202799a90f
Tom Xu <xudotcom@gmail.com>
2017-06-20 00:38:37 -0400

    created name card container and presentational component to list out all words

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Words from './words.json'
+
+class NameCardContainer extends React.Component {
+  state = { words: [] }
+
+  componentDidMount() {
+    fetchWords(words =>
+      this.setState({ words: words }))
+  }
+  render() {
+      return <NameCard words={this.state.words} />
+  }
+}
+
+class NameCard extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.words.map(list =>
+          <li>{list.word}</li>)}
+      </ul>
+      )
+  }
+}
+
+// const NameCard = props =>
+//   (
+//     <ul>
+//       {props.words.map(list => (
+//         <li>{list.word}</li>
+//         ))}
+//     </ul>
+//     )
+
+const fetchWords = cb =>
+  cb(Words)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <NameCardContainer />
+    )
   }
 }
 
-export default App;
+export default App
