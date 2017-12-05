895320a745079e48520edf21cf7aa436e9e1415c
Tom Xu <xudotcom@gmail.com>
2017-12-04 21:32:07 -0500

    add file

---
+++ src/components/ui/revealButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function RevealButton() {
+
+
+    return (
+        <div className='centerButtons'>
+            <button className='revealButton'>
+                Reveal Cards
+            </button>
+        </div>
+    )
+}
+
+export default RevealButton
